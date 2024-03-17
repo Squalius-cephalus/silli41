8a01886db35054c8ad4e2f1ca7e1a965bc189d26
Squalius-cephalus <juho.taskinen@edu.turkuamk.fi>
2024-03-17 14:16:42 +0200

    Initial commit

---
+++ firmware/silli41/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x47, 0x3D, 0x85, 0x27, 0xB9, 0xD7, 0x0A, 0x85}
+#define VIAL_UNLOCK_COMBO_ROWS {0, 1}
+#define VIAL_UNLOCK_COMBO_COLS {0, 1}
